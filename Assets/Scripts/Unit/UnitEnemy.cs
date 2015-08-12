21d5268b7e80cbf26118b356d4e667f3ee84c959
Mokbii <khsong33@gmail.com>
2015-08-13 01:31:12 +0900

    1. 적군 캐릭터 임시 데이터 업데이트

--- Assets/Scripts/Unit/UnitEnemy.cs
+++ Assets/Scripts/Unit/UnitEnemy.cs
@@ -3,10 +3,15 @@ using System.Collections;
 
 public class UnitEnemy : Unit
 {
+	public Animator vAnim;
 	void Start()
 	{
 		GameDataManager.aInstance.AddEnemy(this);
-
+		if (vAnim != null)
+		{
+			vAnim.CrossFade("Running@loop", 0.1f, 0, 0.5f);
+			//vAnim.Play("Running@loop");
+		}
 	}
 	public override void Init()
 	{
